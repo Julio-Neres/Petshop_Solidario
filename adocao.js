8c836d42c2e6cfeebe1873eb1482ee2e29e7c7e4
Julio.Neres <julio.neres@hotmail.com>
2023-11-18 16:13:37 -0300

    atualização da página doação

--- adocao.js
+++ adocao.js
@@ -1,33 +1,60 @@
-let currentSlide = 0;
+let currentSlides = [0, 0, 0];
 
-function showSlide(index) {
-    const slides = document.querySelector('.faixa-carousel');
-    const slideWidth = document.querySelector('.slide-carousel').offsetWidth;
+function showSlide(carouselIndex, index) {
+    const carouselId = `carousel${carouselIndex + 1}`;
+    const slides = document.querySelector(`.faixa-${carouselId}`);
+    const slideWidth = slides.children[0].offsetWidth;
 
     if (index >= 0 && index < slides.children.length) {
-        currentSlide = index;
-        slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+        currentSlides[carouselIndex] = index;
+        slides.style.transform = `translateX(-${index * slideWidth}px)`;
     }
 }
 
-function prevSlide() {
-    showSlide(currentSlide - 1);
+function prevSlide(carouselIndex) {
+    showSlide(carouselIndex, currentSlides[carouselIndex] - 1);
 }
 
-function nextSlide() {
-    showSlide(currentSlide + 1);
+function nextSlide(carouselIndex) {
+    showSlide(carouselIndex, currentSlides[carouselIndex] + 1);
 }
 
-const coracao = document.getElementById('coracao');
 
-let coracaoClicado = false;
+for (let i = 0; i < 3; i++) {
+    const prevButton = document.getElementById(`anterior${i + 1}`);
+    const nextButton = document.getElementById(`proximo${i + 1}`);
 
-coracao.addEventListener('click', function () {
-  coracaoClicado = !coracaoClicado;
+    prevButton.addEventListener('click', () => prevSlide(i));
+    nextButton.addEventListener('click', () => nextSlide(i));
+}
 
-  if (coracaoClicado) {
+const coracao1 = document.querySelector('#coracao1');
+const coracao2 = document.querySelector('#coracao2');
+const coracao3 = document.querySelector('#coracao3');
+
+let coracaoClicado1 = false;
+let coracaoClicado2 = false;
+let coracaoClicado3 = false;
+
+coracao1.addEventListener('click', function () {
+  coracaoClicado1 = !coracaoClicado1;
+  atualizarCor(coracao1, coracaoClicado1);
+});
+
+coracao2.addEventListener('click', function () {
+  coracaoClicado2 = !coracaoClicado2;
+  atualizarCor(coracao2, coracaoClicado2);
+});
+
+coracao3.addEventListener('click', function () {
+  coracaoClicado3 = !coracaoClicado3;
+  atualizarCor(coracao3, coracaoClicado3);
+});
+
+function atualizarCor(coracao, clicado) {
+  if (clicado) {
     coracao.style.color = 'red';
   } else {
     coracao.style.color = 'white';
   }
-});
+}
